acd51dbe4b558dc382ddbe55c0f0c2ae881de346
jeongkpa <jeongkpa@gmail.com>
2024-07-14 06:49:15 +0900

    graph추가

--- lib/features/history/views/history_screen.dart
+++ lib/features/history/views/history_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:animations/animations.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -9,8 +9,6 @@ import 'package:mood_tracker/constants/gaps.dart';
 import 'package:mood_tracker/features/history/view_models/history_view_model.dart';
 import 'package:mood_tracker/features/history/views/emotion_detail_screen.dart';
 import 'package:mood_tracker/features/history/views/setting_screen.dart';
-import 'package:mood_tracker/features/posts/view_models/timeline_view_model.dart';
-import 'package:mood_tracker/features/posts/views/widgets/emotion_ball.dart';
 
 class HistoryScreen extends ConsumerStatefulWidget {
   const HistoryScreen({super.key});
@@ -20,12 +18,9 @@ class HistoryScreen extends ConsumerStatefulWidget {
 }
 
 class _HistoryScreenState extends ConsumerState<HistoryScreen> {
-  // TODO: 상단에 감정 통계
-
-  // TODO: 특정 ball 선택시 modal 로 보여주기
   bool isInit = false;
   late int cntSum;
-  late var cntEmotion;
+  late Map<String, int> cntEmotion;
 
   @override
   void initState() {
@@ -84,11 +79,10 @@ class _HistoryScreenState extends ConsumerState<HistoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("History"),
+          title: const Text("감정 기록 횟수"),
           centerTitle: true,
           actions: [
             GestureDetector(
@@ -139,34 +133,129 @@ class _HistoryScreenState extends ConsumerState<HistoryScreen> {
                             color: Colors.red),
                       ],
                     ),
-                    const Divider(
+                    const SizedBox(
                       height: 20,
-                      thickness: 2,
                     ),
                     Expanded(
-                      child: Container(
-                        alignment: Alignment.bottomCenter,
-                        padding: const EdgeInsets.only(
-                          bottom: 20,
-                        ),
-                        child: Wrap(
-                          alignment: WrapAlignment.end,
-                          verticalDirection: VerticalDirection.up,
-                          children: List.generate(
-                            posts.length,
-                            (index) {
-                              return GestureDetector(
-                                onTap: () => _onTapEmotion(posts[index]),
-                                child: Hero(
-                                  tag: posts[index].id,
-                                  child: EmotionBall(
-                                    name: posts[index].emotionName,
-                                    isCore:
-                                        posts[index].imgs?.isNotEmpty ?? false,
-                                  ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: BarChart(
+                          BarChartData(
+                            alignment: BarChartAlignment.spaceAround,
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            titlesData: FlTitlesData(
+                              show: true,
+                              bottomTitles: AxisTitles(
+                                sideTitles: SideTitles(
+                                  showTitles: true,
+                                  getTitlesWidget:
+                                      (double value, TitleMeta meta) {
+                                    const style = TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    );
+                                    Widget text;
+                                    switch (value.toInt()) {
+                                      case 0:
+                                        text = const Text('Joy', style: style);
+                                        break;
+                                      case 1:
+                                        text =
+                                            const Text('Sadness', style: style);
+                                        break;
+                                      case 2:
+                                        text =
+                                            const Text('Disgust', style: style);
+                                        break;
+                                      case 3:
+                                        text = const Text('Fear', style: style);
+                                        break;
+                                      case 4:
+                                        text =
+                                            const Text('Anger', style: style);
+                                        break;
+                                      default:
+                                        text = const Text('');
+                                        break;
+                                    }
+                                    return SideTitleWidget(
+                                      axisSide: meta.axisSide,
+                                      child: text,
+                                    );
+                                  },
+                                  reservedSize: 28,
                                 ),
-                              );
-                            },
+                              ),
+                              leftTitles: AxisTitles(
+                                sideTitles: SideTitles(
+                                  showTitles: true,
+                                  getTitlesWidget:
+                                      (double value, TitleMeta meta) {
+                                    const style = TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    );
+                                    return Text('${value.toInt()}',
+                                        style: style);
+                                  },
+                                  reservedSize: 40,
+                                ),
+                              ),
+                            ),
+                            gridData: const FlGridData(
+                              show: false,
+                            ),
+                            barGroups: [
+                              BarChartGroupData(
+                                x: 0,
+                                barRods: [
+                                  BarChartRodData(
+                                    toY: cntEmotion["joy"]!.toDouble(),
+                                    color: Colors.yellow.shade600,
+                                  ),
+                                ],
+                              ),
+                              BarChartGroupData(
+                                x: 1,
+                                barRods: [
+                                  BarChartRodData(
+                                    toY: cntEmotion["sadness"]!.toDouble(),
+                                    color: Colors.blue.shade600,
+                                  ),
+                                ],
+                              ),
+                              BarChartGroupData(
+                                x: 2,
+                                barRods: [
+                                  BarChartRodData(
+                                    toY: cntEmotion["disgust"]!.toDouble(),
+                                    color: Colors.green.shade600,
+                                  ),
+                                ],
+                              ),
+                              BarChartGroupData(
+                                x: 3,
+                                barRods: [
+                                  BarChartRodData(
+                                    toY: cntEmotion["fear"]!.toDouble(),
+                                    color: Colors.purple.shade600,
+                                  ),
+                                ],
+                              ),
+                              BarChartGroupData(
+                                x: 4,
+                                barRods: [
+                                  BarChartRodData(
+                                    toY: cntEmotion["anger"]!.toDouble(),
+                                    color: Colors.red.shade600,
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
@@ -185,60 +274,6 @@ class _HistoryScreenState extends ConsumerState<HistoryScreen> {
         Icon(icon, color: color ?? Colors.black, size: 30),
         Text(count.toString(), style: TextStyle(color: color ?? Colors.black)),
       ],
-    );
-  }
-}
-
-class EmotionBall extends StatelessWidget {
-  final String name;
-  final bool isCore;
-
-  const EmotionBall({
-    super.key,
-    required this.name,
-    required this.isCore,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final emotionColors = {
-      "joy": Colors.yellow.shade300,
-      "sadness": Colors.blue.shade200,
-      "disgust": Colors.green.shade200,
-      "fear": Colors.purple.shade200,
-      "anger": Colors.red.shade200,
-    };
-
-    final emotionIcons = {
-      "joy": Icons.sentiment_very_satisfied,
-      "sadness": Icons.sentiment_dissatisfied,
-      "disgust": Icons.sentiment_neutral,
-      "fear": Icons.sentiment_very_dissatisfied,
-      "anger": Icons.sentiment_very_dissatisfied,
-    };
-
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            blurRadius: 5,
-            spreadRadius: 1,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: CircleAvatar(
-        radius: isCore ? 30 : 20,
-        backgroundColor: emotionColors[name],
-        child: Icon(
-          emotionIcons[name],
-          color: Colors.white,
-          size: isCore ? 30 : 20,
-        ),
-      ),
     );
   }
 }
